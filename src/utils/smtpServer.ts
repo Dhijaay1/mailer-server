f82de527306791791902e0a1789771ec5a307ace
mailer2024 <165726606+mailer2024@users.noreply.github.com>
2024-07-09 12:40:10 +0100

    Update smtpServer.ts

--- src/utils/smtpServer.ts
+++ src/utils/smtpServer.ts
@@ -73,7 +73,7 @@ export const emailServer = async (
         from: `<i***@gmail.com> ${emailBody.senderFirstName} ${emailBody.senderLastName}`,
         to: emailBody.recipientEmail,
         subject: config.subject,
-        text: `Hi ${emailBody.recipientFirstName},\n\n${config.content}\n\n\n ${config.greetings}`,
+        text: `Dear ${emailBody.recipientFirstName},\n\n${config.content}\n\n\n ${config.greetings}`,
       };
 
       // Create a promise that resolves after 3 seconds
