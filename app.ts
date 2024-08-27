e9c971c3430a6cf1deac87d5d4daa6580a0b2533
mailer2024 <165726606+mailer2024@users.noreply.github.com>
2024-08-27 23:16:23 +0100

    Update app.ts

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ let clientId: string;
 
 const app: Express = express();
 
-const port = 5000;
+const port = 7000;
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
